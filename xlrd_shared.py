2caa9e6a9fc30c40a7a48d728be28a9a8598dde6
patrickbryant1 <patrick.bryant@live.com>
2018-11-05 11:10:08 +0100

    Add files via upload
    Updated to use openpyxl and argparse.

--- xlrd_shared.py
+++ xlrd_shared.py
@@ -13,8 +13,31 @@ import sys
 import os
 import pdb
 import xlrd
-import xlwt
+import argparse
+import openpyxl
 
+#Arguments for argparse module:
+parser = argparse.ArgumentParser(description = '''
+This is a program that takes an xl workbook, reads it and uses its
+sheets to perform filtering as specified by text files.
+
+-Remember that you cannot write too many rows to an excel sheet,
+there is a limitation!
+''')
+ 
+parser.add_argument('workbook_r', nargs=1, type= str,
+                  default=sys.stdin, help = 'path to excel file with variants to be opened')
+
+
+parser.add_argument('ref_dbs', nargs=1, type= str,
+                  default=sys.stdin, help = '''path to text file containing column positions
+                  for ref_dbs to filter on.''')
+
+parser.add_argument('zygosity_positions', nargs=1, type= str,
+                  default=sys.stdin, help = '''path to text file containing
+                  zygosity_positions to filter on.''')
+
+###########################################################################################
 #Functions
 
 def text_to_list(file_name):
@@ -120,35 +143,36 @@ def filter_sheet(workbook_r, name, ref_dbs, zygosity_positions):
     '''
     
     #Create an excel workbook and a sheet to write to
-    workbook_w = xlwt.Workbook()
-    right_zyg = True
+    workbook_w = openpyxl.Workbook()
+    sheet_w = workbook_w.active #Get active sheet
+    sheet_w.title = 'maf_filtered'
+
     shared = False
 
     for num in range(0, 1):#workbook_r.nsheets):
-        xl_sheet = workbook_r.sheet_by_index(num) #Open sheet_num
-        sheet_w = workbook_w.add_sheet('Sheet_'+str(num+1)) #Create sheet_num into workbook_w
+        sheet_r = workbook_r.sheet_by_index(num) #Open sheet_num
 	
         row_idx = 0 #Row to transfer from
-        row_n = 0 #To keep track of which row to write to
+        row_n = 1 #To keep track of which row to write to
         #transfer first row
-        write_to_sheet(row_idx, sheet_w, xl_sheet, row_n)
+        write_to_sheet(row_idx, sheet_w, sheet_r, row_n)
 
 
-        for row_idx in range(1, xl_sheet.nrows):
+        for row_idx in range(1, sheet_r.nrows):
             above_t = False
  	    for ref_db in ref_dbs:
                 ref_db = ref_db.split(' ') #split each list item on space
-                MAF = encode_ascii(xl_sheet, row_idx, int(ref_db[1]))#filter on reference databases MAFs
+                MAF = encode_ascii(sheet_r, row_idx, int(ref_db[1]))#filter on reference databases MAFs
                 if MAF and MAF != '.': #Checks if MAF is empty or NA
                     MAF = float(MAF)
                     if MAF > float(ref_db[3]): #Filtering on MAF above threshold
                         above_t = True
             
-            shared = find_shared(zygosity_positions, xl_sheet, row_idx, above_t)
+            shared = find_shared(zygosity_positions, sheet_r, row_idx, above_t)
             if shared == True:
                 
                     row_n += 1
-                    write_to_sheet(row_idx, sheet_w, xl_sheet, row_n)
+                    write_to_sheet(row_idx, sheet_w, sheet_r, row_n)
                     
             else:
                 continue
@@ -157,42 +181,44 @@ def filter_sheet(workbook_r, name, ref_dbs, zygosity_positions):
 
     return None
 
-def write_to_sheet(row_idx, sheet_w, xl_sheet, row_n):
+def write_to_sheet(row_idx, sheet_w, sheet_r, row_n):
     '''A function that writes data into a sheet in the excel workbook.
     Input: row_idx, sheet_w, xl_sheet
     Output: None
     '''
 
     #Iterate over all columns
-    for col_idx in range(0, xl_sheet.ncols):
-        cell = xl_sheet.cell(row_idx, col_idx).value
+    for col_idx in range(0, sheet_r.ncols):
+        cell = sheet_r.cell(row_idx, col_idx).value
         if type(cell) == float:
-        	cell = str(cell)
+            	cell = str(cell) 
         cell = cell.encode('ascii','ignore')
-        sheet_w.write(row_n, col_idx, cell)
+        sheet_w.cell(row = row_n, column = col_idx+1).value = cell #Openpyxl is not zero indexed, but xlrd is
 
     return None
-                
+    
+    
 #Main program
+args = parser.parse_args()
 
 try:
-    workbook_r = xlrd.open_workbook(sys.argv[1])
+    workbook_r = xlrd.open_workbook(args.workbook_r[0])
     #The opened workbook is closed automatically after use
     
     #Checks if file is empty
-    if os.stat(sys.argv[1]).st_size == 0:
+    if os.stat(args.workbook_r[0]).st_size == 0:
        print "Empty file."       
 except IOError:
-    print 'Cannot open', sys.argv[1]
+    print 'Cannot open', args.workbook_r[0]
 else:
 
 
     #Arguments to filter on
-    ref_dbs = text_to_list(sys.argv[2])
-    zygosity_positions = text_to_list(sys.argv[3])
+    ref_dbs = text_to_list(args.ref_dbs[0])
+    zygosity_positions = text_to_list(args.zygosity_positions[0])
     
     #Perform the filtering
-    name = str(sys.argv[1]).split('/')[-1:] #name is only the last part of path
+    name = str(args.workbook_r[0]).split('/')[-1:] #name is only the last part of path
                                             #name is a list here
     filter_sheet(workbook_r, name, ref_dbs, zygosity_positions)
 
